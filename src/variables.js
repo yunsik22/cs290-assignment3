f4747a1d599a715701c3a8176c23b28605c6f89d
yunsik22 <choiy@onid.orst.edu>
2015-02-01 20:15:42 -0800

    index.html added

---
+++ src/variables.js
@@ -0,0 +1,56 @@
+/*
+Input:
+a: a whole, positive number
+
+Output:
+plus5: a number that is the sum of 5 and `a`
+asString: a string that is just `a` converted to a string
+yourNumberIs: a string that says "Your Number is `x`." where `x` is replaced by
+in input `a`
+a: the original a number
+*/
+function variableModification(a) {
+  var plus5;
+  var asString;
+  var yourNumberIs;
+  //your code here
+  plus5 = a + 5;
+  asString = a.toString();
+  yourNumberIs = 'Your Number is ' + a;
+  
+  //end your code
+  return [plus5, asString, yourNumberIs, a];
+}
+
+/*
+Input:
+b: could be anything
+
+Output:
+return true if b is a primitive string value (also known as a string literal),
+false otherwise
+*/
+function isString(b) {
+  //your code here
+  if (typeof b === 'string' || typeof b === 'undefined')
+    return true;
+  
+  return false;
+  //end your code
+}
+
+/*
+Input:
+c: could be anything
+
+Output:
+return true if c is null, false otherwise
+*/
+function isNull(c) {
+  //your code here
+  if (typeof c === undefined || c === null)
+    return true;  
+  
+  return false;
+  //end your code
+}
